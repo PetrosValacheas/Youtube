3b0c4e73053ca2222a5d0721944b0a80dff68622
PetrosVal <petrosva12gmail.com>
2020-12-25 16:29:02 +0200

    Project Initialization

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello</h1>
+
+        {/*Header*/}
+        {/*SideBar*/}
+        {/*RecommededVideos*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDt5mmcbkRo_sTpCGo5h9kqt8Ky4FIv5bg",
+  authDomain: "clone-337ee.firebaseapp.com",
+  projectId: "clone-337ee",
+  storageBucket: "clone-337ee.appspot.com",
+  messagingSenderId: "234371813780",
+  appId: "1:234371813780:web:ae81768ba567985e4ebf81",
+  measurementId: "G-E6KSLGPK59"
+};
